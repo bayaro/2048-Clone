c036ddf4cec96f212d3d6155f458d6c1bd8ede6b
Yaroslav Bo <baya@rodovid.org>
2015-08-19 00:40:27 +0300

    + shift left

--- processor.js
+++ processor.js
@@ -2,8 +2,14 @@
 // we don't use additional array because of in any case we should check/move
 // numbers throw cells, so we should check as current as neibour cell
 
+COLS = ROWS = 5;
+
+function game_over() {
+    alert( "Game Over" );
+}
+
 $(document).ready( function() {
-    $('.map').add_boxes({'rows':5,'cols':5}).add_number().add_number();
+    $('.map').add_boxes({'rows':ROWS,'cols':COLS}).add_number().add_number();
     $(window).resize();
 });
 
@@ -16,13 +22,16 @@ $(window).resize( function() {
     case 39: // right
     case 40: // down
         break;
+    case 27:
+        e.preventDefault();
+        $('.map').add_boxes({'rows':ROWS,'cols':COLS}).add_number().add_number();
+        return;
     default:
         return;
     }
     e.preventDefault();
     $('.map').shift();
 });
-
 
 (function($) {
     $.fn.add_boxes = function( customOptions ) {
@@ -34,10 +43,12 @@ $(window).resize( function() {
         $.extend( options, customOptions );
 
         // clear map, if, in future, we will want to change rows/cols interactivelly
-        map = $(this).html( '' ).attr( 'cell_count', options.rows * options.cols );
+        map = $(this).html( '' ).attr( 'cell_count', options.rows * options.cols )
+            .attr( 'rows', options.rows ). attr( 'cols', options.cols );
         boxH = ( 100 / options.rows ) + '%';
         boxW = ( 100 / options.cols ) + '%';
         fSize = ( 60 / options.rows ) + 'vh';
+        i = 2;
         for( r = 0; r < options.rows; r++ ) {
             row = $( '<div class="row">' ).css( 'height', boxH ).appendTo( map );
             for( c = 0; c < options.cols; c++ ) {
@@ -50,12 +61,47 @@ $(window).resize( function() {
     };
 
     $.fn.add_number = function() {
-        $(this).find('.box#n_' + Math.floor( Math.random() * $(this).attr( 'cell_count' ) ) )
-            .text('2'); // TODO random number (?)
+        sc = $(this).attr( 'cell_count' );
+        n = Math.floor( Math.random() * sc );
+        checked = 0;
+        while( $(this).find('.box#n_' + n).text() && ++checked <= sc ) {
+            if ( ++n >= sc ) n = 0;
+        }
+        if ( checked >= sc ) {
+            game_over();
+            return;
+        }
+        $(this).find('.box#n_' + n ).text('2'); // TODO random number (?)
         return $(this);
     };
 
     $.fn.shift = function() {
+        map = $(this);
+        rows = parseInt( map.attr( 'rows' ) );
+        cols = parseInt( map.attr( 'cols' ) );
+        for( r = 0; r < rows; r++ ) {
+            for( c = 1; c < cols; c++ ) {
+                ci = r * cols + c;
+                check_prev( ci, ci - 1 );
+            }
+        }
+        $(this).add_number();
         return $(this);
     };
 })(jQuery);
+
+function check_prev( ci, pi ) {
+    // in this algorithm double summing occures !!!
+    cur = $('#n_' + ci );
+    ct = cur.text();
+    if ( ct == '' ) return;
+    prev = $('#n_' + pi );
+    switch( prev.text() ) {
+    case ct: ct = 2 * parseInt( ct );
+    case '':
+        break;
+    default: return;
+    }
+    prev.text( ct );
+    cur.text( '' )
+}
