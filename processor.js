443533a45ef12f3d1c4b7b55633ae18355c96ca1
Yaroslav Bo <baya@rodovid.org>
2015-08-20 03:07:46 +0300

    + game over functionality

--- processor.js
+++ processor.js
@@ -5,7 +5,7 @@
 COLS = ROWS = 4;
 
 function game_over() {
-    alert( "Game Over" );
+    $('.gameover').css('display','block');
 }
 
 $(document).ready( function() {
@@ -14,7 +14,15 @@ $(document).ready( function() {
 });
 
 $(window).resize( function() {
-    $('.map').css( 'width', $('.map').height() + 'px' );
+    m = $('.map');
+    h = m.height() + 'px';
+    m.css( 'width', h );
+    o = m.offset();
+    t = parseFloat( h );
+    h = 0.75 * t + 'px';
+    l = t / 8 + o.left +'px';
+    t = t / 8 + o.top + 'px';
+    $('.gameover').css({'width':h, 'height':h, 'left':l, 'top':t});
 }).keydown( function(e) {
     switch(e.which){
     case 37: // left
@@ -30,7 +38,9 @@ $(window).resize( function() {
         return;
     }
     e.preventDefault();
-    $('.map').shift( e.which - 37 );
+    map = $('.map');
+    map.shift( e.which - 37 );
+    if ( map.data('moved') ) map.add_number();
 });
 
 (function($) {
@@ -67,11 +77,17 @@ $(window).resize( function() {
         while( $(this).find('.box#n_' + n).text() && ++checked <= sc ) {
             if ( ++n >= sc ) n = 0;
         }
-        if ( checked >= sc ) {
-            game_over();
-            return;
-        }
         $(this).find('.box#n_' + n ).text('2'); // TODO random number (?)
+
+        test = $('.map-test');
+        if ( test.length == 0 ) {
+            test = $(this).clone().removeClass('map').addClass('map-test').css('display','none').appendTo('body');
+        } else test.html( $(this).html() );
+        for( i = 0; i <= 3; i++ ) {
+            test.shift( i );
+            if ( test.data('moved') ) return $(this);
+        }
+        game_over();
         return $(this);
     };
 
@@ -79,74 +95,72 @@ $(window).resize( function() {
         map = $(this);
         rows = parseInt( map.attr( 'rows' ) );
         cols = parseInt( map.attr( 'cols' ) );
-        check_prev = one_step;
-        check_prev = roll_down;
-        moved = false;
+        map.check_prev = map.one_step;
+        map.check_prev = map.roll_down;
+        map.data('moved',0);
         switch( dir ) {
         case 0: // left
             for( r = 0; r < rows; r++ ) for( c = 1; c < cols; c++ ) {
-                if ( m = check_prev( r * cols, cols, r, c, -1  ) ) moved = true;
+                map.check_prev( r * cols, cols, r, c, -1  );
             }
             break;
         case 2: // right
             for( r = 0; r < rows; r++ ) for( c = cols-2; c >= 0; c-- ) {
-                if ( m = check_prev( ( r + 1 ) * cols - 1, cols, r, c, 1  ) ) moved = true;
+                map.check_prev( ( r + 1 ) * cols - 1, cols, r, c, 1  );
             }
             break;
         case 1: // up
             for( c = 0; c < cols; c++ ) for( r = 1; r < rows; r++ ) {
-                if ( m = check_prev( c, cols, r, c, -cols  ) ) moved = true;
+                map.check_prev( c, cols, r, c, -cols  );
             }
             break;
         case 3: // down
             for( c = 0; c < cols; c++ ) for( r = rows-2; r >= 0; r-- ) {
-                if ( m = check_prev( ( rows - 1 ) * cols + c, cols, r, c, cols  ) ) moved = true;
+                map.check_prev( ( rows - 1 ) * cols + c, cols, r, c, cols );
             }
             break;
-        default: return $(this);
         }
-        if ( moved ) $(this).add_number();
         return $(this);
     };
-})(jQuery);
 
-var roll_down = function roll_down( down, cols, r, c, step ) {
-    ci = r * cols + c;
-    cur = $('#n_' + ci );
-    ct = cur.text();
-    if ( ct == '' ) return;
-    pi = down;
-    while( pi != ci ) {
-        prev = $('#n_' + pi );
+    $.fn.roll_down = function roll_down( down, cols, r, c, step ) {
+        ci = r * cols + c;
+        cur = $(this).find('#n_' + ci );
+        ct = cur.text();
+        if ( ct == '' ) return $(this);
+        pi = down;
+        while( pi != ci ) {
+            prev = $(this).find('#n_' + pi );
+            switch( prev.text() ) {
+            case ct: ct = 2 * parseInt( ct );
+            case '':
+                break;
+            default:
+                pi -= step;
+                continue;
+            }
+            prev.text( ct );
+            cur.text( '' );
+            $(this).data('moved',1);
+            break;
+        }
+        return $(this);
+    };
+
+    $.fn.one_step = function one_step( down, cols, r, c, step ) {
+        ci = r * cols + c;
+        cur = $(this).find('#n_' + ci );
+        ct = cur.text();
+        if ( ct == '' ) return $(this);
+        pi = ci + step;
+        prev = $(this).find('#n_' + pi );
         switch( prev.text() ) {
         case ct: ct = 2 * parseInt( ct );
         case '':
-            break;
-        default:
-            pi -= step;
-            continue;
+            prev.text( ct );
+            cur.text( '' );
+            $(this).data('moved',1);
         }
-        prev.text( ct );
-        cur.text( '' );
-        return true;
-    }
-}
-
-var one_step = function one_step( down, cols, r, c, step ) {
-    ci = r * cols + c;
-    cur = $('#n_' + ci );
-    ct = cur.text();
-    if ( ct == '' ) return;
-    pi = ci + step;
-    prev = $('#n_' + pi );
-    switch( prev.text() ) {
-    case ct: ct = 2 * parseInt( ct );
-    case '':
-        break;
-    default: return;
-    }
-    prev.text( ct );
-    cur.text( '' );
-    return true;
-}
-
+        return $(this);
+    };
+})(jQuery);
